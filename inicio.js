29b1df1bd4504f386fabd76b6a653efc27d7c0ca
franciscoalfar@gmail.com <*Fran2811*>
2024-03-11 21:26:53 -0300

    cors option

--- inicio.js
+++ inicio.js
@@ -12,12 +12,11 @@ const app = express();
 const puerto = 4000;
 
 //configurar cors
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://franciscoalfaro.cl');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
+app.use(cors({
+    origin: 'http://franciscoalfaro.cl',
+    allowedHeaders: ['Authorization', 'Content-Type']
+}));
+
 //conertir los datos del body a obj js
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
